246d2beb18a4cc401d140e760a0e921c29314ef9
OpenShift guest <abrar.here@gmail.com>
2013-04-18 14:32:18 -0400

    hack general assembly

--- web.js
+++ web.js
@@ -1,12 +1,63 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+var http = require("http");
+var querystring = require("querystring");
+//var process = require("process");
 
 app.get('/', function(request, response) {
   response.send('Hello World! Here I come');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+//app.listen(port, function() {
+  //console.log("Listening on " + port);  
+//});
+
+var targetHost = "go.generalassemb.ly";
+var targetPath = "/l/19312/2013-04-03/2v5w9";
+var options = {
+    host: targetHost,
+    post: 80,
+    path:targetPath,
+    method:'POST'
+};
+
+var paramsJson = {
+  email  :'aahmed@internetbrands.com',
+  metro:'new-york-city',
+  checkbox:'on'
+};
+var params = querystring.stringify(paramsJson);
+console.log(params);
+//process.exit(1);
+
+var headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Content-Length': params.length,
+    'Origin': 'https://generalassemb.ly',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache'
+    
+};
+var req = http.request(options,function(response) {
+    console.log('STATUS: ' + response.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(response.headers));
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
 });
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+req.headers = headers;
+req.write(params);
+req.end();
